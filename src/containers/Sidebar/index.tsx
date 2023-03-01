ab9d41111fa746d23b0c0d65601753e11cd97113
JC-Aires <jcaires63@gmail.com>
2023-03-01 11:35:09 -0300

    Construindo a estrutura e a seção "Sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img
+      src="https://github.com/JC-Aires.png"
+      alt="Foto do usuário Jose Carlos Aires"
+    />
+    <Titulo fontSize={20}>J C Aires</Titulo>
+  </aside>
+)
+
+export default Sidebar
