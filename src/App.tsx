d2d823b3afe24c72421c66326a5216c3343cee62
JC-Aires <jcaires63@gmail.com>
2023-03-02 09:25:07 -0300

    Finalizando com a troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import Sidebar from './containers/Sidebar'
 import EstiloGlobal, { Container } from './styles'
 import Sobre from './containers/Sidebar/Sobre'
 import Projetos from './containers/Projetos'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [usaTemaDark, setUsaTemaDark] = useState(false)
+
+  function trocaTema() {
+    setUsaTemaDark(!usaTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={usaTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
